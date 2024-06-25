da4d523759e63ee2bffd34d76d885c5963c9feef
Yahya Yağmur <yahyaygmr@gmail.com>
2024-06-25 15:51:34 +0300

    Add project files.

---
+++ DapperTest/Services/Abstracts/Category/ICategoryService.cs
@@ -0,0 +1,13 @@
+﻿using DapperTest.Dtos.CategoryDtos;
+
+namespace DapperTest.Services.Abstracts.Category
+{
+    public interface ICategoryService
+    {
+        Task<List<ResultCategoryDto>> GetAllCategoryAsync();
+        Task CreateCategoryAsync(CreateCategoryDto dto);
+        Task DeleteCategoryAsync(int id);
+        Task UpdateCategoryAsync(UpdateCategoryDto dto);
+        Task<GetByIdCategoryDto> GetCategoryAsync(int id);
+    }
+}
